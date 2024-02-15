d963ea8dc421649a2ad1d1592113f89aa0a4d414
Merve Artıran <martiran@etu.edu.tr>
2024-02-15 12:31:39 +0100

    project completed

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const inputs = Array.from(inputElement);
 console.log(inputs);
 
 function handleUpdate() {
-    console.log(this);
-    console.log(this.dataset)
+    // console.log(this);
+    // console.log(this.dataset)
     const suffix = this.dataset.sizing || '';
-    console.log(suffix);
+    // console.log(suffix);
     //this.dataset dediğimizde html kısmında sadece data-sizing verdiğimiz için onu alıyor.Ama data-weigth veya başka data özellikleri ekleseydik hepsini alıyordu.
     //Burada data-sizing olan iki input elementinin sizing değerleri ve color da data değeri olmadığı için veya "" dedik colordaki eventi de aldık.
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
